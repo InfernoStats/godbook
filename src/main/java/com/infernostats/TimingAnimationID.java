42b3a8f30226123d81f9a8f15f8f090e91f102e0
Nick Bulischeck <nbulischeck@users.noreply.github.com>
2025-05-31 13:38:48 -0400

    Add Soulflame Horn

--- src/main/java/com/infernostats/TimingAnimationID.java
+++ src/main/java/com/infernostats/TimingAnimationID.java
@@ -1,6 +1,5 @@
 package com.infernostats;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -19,6 +18,8 @@ public class TimingAnimationID {
   public static final int BOOK_OF_LAW_OR = 9440;
   public static final int BOOK_OF_DARKNESS = 7155;
   public static final int BOOK_OF_DARKNESS_OR = 9441;
+  public static final int SOULFLAME_HORN = 12158;
+  public static final int SOULFLAME_HORN_MISS = 12159;
 
   public static final int EMOTE_YES = 855;
   public static final int EMOTE_NO = 856;
@@ -26,31 +27,29 @@ public class TimingAnimationID {
   public static final int EMOTE_THINK = 857;
   public static final int EMOTE_DANCE = 866;
 
-  private static final List<Integer> godbookAnimationIDs = new ArrayList<>(
-      Arrays.asList(
-          HOLY_BOOK,
-          HOLY_BOOK_OR,
-          UNHOLY_BOOK,
-          UNHOLY_BOOK_OR,
-          BOOK_OF_BALANCE,
-          BOOK_OF_BALANCE_OR,
-          BOOK_OF_WAR,
-          BOOK_OF_WAR_OR,
-          BOOK_OF_LAW,
-          BOOK_OF_LAW_OR,
-          BOOK_OF_DARKNESS,
-          BOOK_OF_DARKNESS_OR
-      )
+  private static final List<Integer> godbookAnimationIDs = Arrays.asList(
+      HOLY_BOOK,
+      HOLY_BOOK_OR,
+      UNHOLY_BOOK,
+      UNHOLY_BOOK_OR,
+      BOOK_OF_BALANCE,
+      BOOK_OF_BALANCE_OR,
+      BOOK_OF_WAR,
+      BOOK_OF_WAR_OR,
+      BOOK_OF_LAW,
+      BOOK_OF_LAW_OR,
+      BOOK_OF_DARKNESS,
+      BOOK_OF_DARKNESS_OR,
+      SOULFLAME_HORN,
+      SOULFLAME_HORN_MISS
   );
 
-  private static final List<Integer> emoteAnimationIDs = new ArrayList<>(
-      Arrays.asList(
-        EMOTE_YES,
-        EMOTE_NO,
-        EMOTE_SPIN,
-        EMOTE_THINK,
-        EMOTE_DANCE
-      )
+  private static final List<Integer> emoteAnimationIDs = Arrays.asList(
+      EMOTE_YES,
+      EMOTE_NO,
+      EMOTE_SPIN,
+      EMOTE_THINK,
+      EMOTE_DANCE
   );
 
   private static final List<Integer> timingAnimationIDs = Stream.of(
